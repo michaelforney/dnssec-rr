e59635a36989edcacb92517a9868a34b7b3f6148
Michael Forney <mforney@mforney.org>
2021-05-04 12:47:02 -0700

    base{16,64}: Don't assume character set is ASCII
    C only requires that '0'...'9' have sequential values, so for base16
    use a switch for 'a'...'f', and add explicit designators in the
    base64 table.

--- base16.c
+++ base16.c
@@ -4,12 +4,16 @@
 static int
 base16_value(int c)
 {
-	if ('A' <= c && c <= 'Z')
-		return c - 'A' + 10;
-	if ('a' <= c && c <= 'z')
-		return c - 'a' + 10;
 	if ('0' <= c && c <= '9')
 		return c - '0';
+	switch (c) {
+	case 'a': case 'A': return 10;
+	case 'b': case 'B': return 11;
+	case 'c': case 'C': return 12;
+	case 'd': case 'D': return 13;
+	case 'e': case 'E': return 14;
+	case 'f': case 'F': return 15;
+	}
 	errx(1, "invalid base16 character '%c'", c);
 }
 
